a23ca4bc9bd6d922497356f45ce3afe25c876366
Gary Cheung <garyc2232@gmail.com>
2024-06-15 17:41:47 +0800

    add database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthCheckModule } from './modules/healthCheck/healthCheck.module';
 import { TagModule } from './modules/tag/tag.module';
+import { StatusModule } from './modules/status/status.module';
+import { DatabaseModule } from './modules/providers/database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import config from './config/config';
 
 @Module({
-  imports: [HealthCheckModule, TagModule],
+  imports: [
+    HealthCheckModule,
+    TagModule,
+    StatusModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      load: [config],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
